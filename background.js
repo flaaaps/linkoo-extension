1bad40ede323d1ff9ddff77511d469d7ccbdbfbc
flaaaps <julianwening4@gmail.com>
2021-02-20 01:13:22 +0100

    :rocket: Improve overall validation & error handling

--- background.js
+++ background.js
@@ -4,75 +4,90 @@ const socket = io(API_BASE_URL, { transports: ['websocket'] });
 
 // generates a extension id
 let id = () => {
-    let s4 = () => {
-        return Math.floor((1 + Math.random()) * 0x10000)
-            .toString(16)
-            .substring(1);
-    };
-    return s4() + s4();
+	let s4 = () => {
+		return Math.floor((1 + Math.random()) * 0x10000)
+			.toString(16)
+			.substring(1);
+	};
+	return s4() + s4();
 };
 
 const linkooExtensionId = id();
 
 let messagePort;
 chrome.runtime.onConnect.addListener(function (port) {
-    console.log('Port:', port);
-    if (port.name === 'linkoo-message-channel') {
-        messagePort = port;
+	console.log('Port:', port);
+	if (port.name === 'linkoo-message-channel') {
+		messagePort = port;
 
-        messagePort.onDisconnect.addListener(() => {
-            console.log('Message port disconnected.');
-        });
-    }
-    messagePort.onMessage.addListener(async function (result) {
-        console.log('Result:', result);
-        if (messagePort.name === 'linkoo-message-channel') {
-            if (result.type === 'login') {
-                console.log('Got login call!!');
-                if (result.details?.name) {
-                    socket.emit('login', result.details.name);
-                } else {
-                    chrome.storage.sync.get(['user'], (data) => {
-                        if (data.user.name) socket.emit('login', data.user.name);
-                    });
-                }
-            } else if (result.type === 'disconnect') {
-                removeUser();
-                socket.emit('leave');
-                messagePort.postMessage({ type: 'logout' });
-            } else if (result.type === 'user') {
-                chrome.storage.sync.get(['user'], (data) => {
-                    console.log('Got user from storage:', data.user);
-                    messagePort.postMessage({ type: 'user', details: { user: data.user } });
-                });
-            }
-        } else if (messagePort.name === 'linkoo-notifier') {
-            messagePort.postMessage({ port: 'linkoo-notifier', message: { type: 'extensionId', id: linkooExtensionId } });
-        }
-    });
+		messagePort.onDisconnect.addListener(() => {
+			console.log('Message port disconnected.');
+		});
+	}
+	messagePort.onMessage.addListener(async function (result) {
+		console.log('Result:', result);
+		if (messagePort.name === 'linkoo-message-channel') {
+			if (result.type === 'login') {
+				console.log('Got login call!!');
+				console.log(result.details?.name, !!result.details?.name);
+				if (!!result.details?.name) {
+					console.log('Result details name true!');
+					socket.emit('login', result.details.name);
+				} else {
+					console.log('Result details name false, fallback!');
+					chrome.storage.sync.get(['user'], data => {
+						if (!!data?.user || !!data?.user?.name)
+							socket.emit('login', data.user.name);
+					});
+				}
+			} else if (result.type === 'disconnect') {
+				removeUser();
+				socket.emit('leave');
+				messagePort.postMessage({ type: 'logout' });
+			} else if (result.type === 'user') {
+				chrome.storage.sync.get(['user'], data => {
+					console.log('Got user from storage:', data.user);
+					messagePort.postMessage({ type: 'user', details: { user: data.user } });
+				});
+			}
+		} else if (messagePort.name === 'linkoo-notifier') {
+			messagePort.postMessage({
+				port: 'linkoo-notifier',
+				message: { type: 'extensionId', id: linkooExtensionId }
+			});
+		}
+	});
 });
 
-socket.on('login', (data) => {
-    chrome.storage.sync.set({ user: data.user }, null);
-    console.log('The port zu dem Zeitpunkt:', messagePort);
-    messagePort.postMessage({ type: 'login', details: { user: data.user } });
-    console.log(data, 'Socket login data');
+socket.on('login', data => {
+	console.log('The port zu dem Zeitpunkt:', messagePort);
+	console.log(data, 'Socket login data');
+	if (data !== 'Error while validating user') {
+		// improve message
+		chrome.storage.sync.set({ user: data.user }, null);
+		messagePort.postMessage({ type: 'login', details: { user: data.user } });
+	} else {
+		messagePort.postMessage({ type: 'error.auth', details: { message: data } });
+	}
 });
 
-socket.on('leave', (data) => {
-    messagePort.postMessage({ type: 'logout', details: { success: data.success } });
+socket.on('leave', data => {
+	messagePort.postMessage({ type: 'logout', details: { success: data.success } });
 });
 
 function removeUser() {
-    chrome.storage.sync.set({ user: {} });
-    // window.location.reload();
-    console.log('Removed user from storage.');
+	chrome.storage.sync.set({ user: {} });
+	// window.location.reload();
+	console.log('Removed user from storage.');
 }
 
-socket.on('message', (data) => {
-    console.log('Received message:', data);
-    const urlRegex = RegExp('(http|https)://[a-zA-Z0-9-.]+.[a-zA-Z]{2,3}(/S*)?');
-    let url = `https://google.com/search?q=${data.content}`;
-    if (!!data.content.match(urlRegex)) url = data.content;
-    chrome.tabs.create({ url: url });
+socket.on('message', data => {
+	console.log('Received message:', data);
+	const urlRegex = RegExp('(http|https)://[a-zA-Z0-9-.]+.[a-zA-Z]{2,3}(/S*)?');
+	let url = `https://google.com/search?q=${data.content}`;
+	const contentParts = data.content.split(' ');
+	contentParts.forEach(part => {
+		if (!!part.match(urlRegex)) url = part;
+	});
+	chrome.tabs.create({ url: url });
 });
--- scripts/popup.js
+++ scripts/popup.js
@@ -4,79 +4,95 @@ const loginInfo = document.getElementById('login-info');
 
 const accountInfo = document.getElementById('account-info');
 const logoutBtn = document.getElementById('logout-btn');
+const nameInput = document.getElementById('login-name-input');
 
 const port = chrome.runtime.connect({
-    name: 'linkoo-message-channel',
+	name: 'linkoo-message-channel'
 });
 
 // Listening for authentication events
-loginForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const nameInput = document.getElementById('login-name-input');
-    port.postMessage({ type: 'login', details: { name: nameInput.value } });
+loginForm.addEventListener('submit', e => {
+	e.preventDefault();
+	port.postMessage({ type: 'login', details: { name: nameInput.value } });
 });
 logoutBtn.addEventListener('click', () => {
-    port.postMessage({ type: 'disconnect' });
+	port.postMessage({ type: 'disconnect' });
 });
 
 port.onMessage.addListener(function (data) {
-    console.log('Popup port result!:', data);
-    if (data.type === 'login') {
-        console.log('New login!');
-        showUserDetails(data);
-    } else if (data.type === 'logout') {
-        showLoginDetails();
-        chrome.storage.sync.set({ loggedIn: false }, null);
-    } else if (data.type === 'user') {
-        console.log('User data received.');
-        console.log('Received user data:', data);
-        if (!data.details.user.name) {
-            showLoginDetails();
-        } else {
-            showUserDetails(data);
-            console.log('Showed user info');
-            chrome.storage.sync.get(['loggedIn'], (data) => {
-                console.log("Let's see if you are logged in:", data);
-                if (!data.loggedIn) {
-                    chrome.storage.sync.set({ loggedIn: true }, null);
-                    port.postMessage({ type: 'login', details: { name: data.details.user.name } });
-                }
-            });
-        }
-    }
+	console.log('Popup port result!:', data);
+	if (data.type === 'login') {
+		console.log('New login!');
+		showUserDetails(data);
+	} else if (data.type === 'logout') {
+		showLoginDetails();
+		chrome.storage.sync.set({ loggedIn: false }, null);
+	} else if (data.type === 'user') {
+		console.log('User data received.');
+		console.log('Received user data:', data);
+		if (!data?.details?.user?.name) {
+			showLoginDetails();
+		} else {
+			showUserDetails(data);
+			const userData = data;
+			console.log('Showed user info');
+			chrome.storage.sync.get(['loggedIn'], data => {
+				console.log("Let's see if you are logged in:", data);
+				if (!data.loggedIn) {
+					chrome.storage.sync.set({ loggedIn: true }, null);
+					console.log(userData, 'DATA!!!');
+					port.postMessage({
+						type: 'login',
+						details: { name: userData.details.user.name }
+					});
+				}
+			});
+		}
+	} else if (data.type === 'error.auth') {
+		handleAuthError(data.details.message);
+	}
 });
 
-function showUserDetails(data) {
-    loginInfo.style.display = 'none';
-    accountInfo.style.display = 'block';
-    setTimeout(() => (accountInfo.style.opacity = '1'), 10);
+function handleAuthError(error) {
+	const authErrorContainer = document.getElementById('auth-error-feedback');
+	// authErrorContainer.innerText = error;
+	// authErrorContainer.style.opacity = '1';
+	console.log('Handling auth error.');
+	authErrorContainer.innerText = error;
+	nameInput.classList.add('is-invalid');
+}
 
-    const accountName = document.getElementById('account-info__name');
-    accountName.innerHTML = data.details.user.name;
+function showUserDetails(data) {
+	loginInfo.style.display = 'none';
+	accountInfo.style.display = 'block';
+	setTimeout(() => (accountInfo.style.opacity = '1'), 10);
+
+	const accountName = document.getElementById('account-info__name');
+	accountName.innerHTML = data.details.user.name;
 }
 
 function showLoginDetails() {
-    loginInfo.style.display = 'block';
-    accountInfo.style.display = 'none';
-    setTimeout(() => (loginInfo.style.opacity = '1'), 10);
+	loginInfo.style.display = 'block';
+	accountInfo.style.display = 'none';
+	setTimeout(() => (loginInfo.style.opacity = '1'), 10);
 }
 
-port.postMessage({ type: 'login' });
+// port.postMessage({ type: 'login' });
 port.postMessage({ type: 'user' });
 
-addEventListener(
-    'unload',
-    function (event) {
-        background.console.log(event.type);
-        port.disconnect();
-    },
-    true
-);
+// addEventListener(
+// 	'unload',
+// 	function (event) {
+// 		background.console.log(event.type);
+// 		port.disconnect();
+// 	},
+// 	true
+// );
 
 // get saved user & validate
 
 // actually open hrefs
-const clickedHref = (e) => chrome.tabs.create({ url: e.target.href });
-document.querySelectorAll('a[href^="http"]').forEach((d) => d.addEventListener('click', clickedHref));
+const clickedHref = e => chrome.tabs.create({ url: e.target.href });
+document.querySelectorAll('a[href^="http"]').forEach(d => d.addEventListener('click', clickedHref));
 
 // https://www.google.com/search?q=heeyjoshi
